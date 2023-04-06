d991e9d5ca7488fe4631727a724f76db5ac4c9bb
Angel Vieira <correiaa129@gmail.com>
2023-04-05 21:32:59 -0300

    Subindo exercício adicionando animação fluida

--- js/index.js
+++ js/index.js
@@ -4,19 +4,20 @@ let seta = document.querySelectorAll('.texto-e-seta')
 
 setaClique.forEach(function(item, index){
   item.addEventListener("click", function ()  {
-
-  let estaOculto = conteudos[index].classList.contains('none')
+  let estaOculto = !conteudos[index].classList.contains('conteudo-mostrado')
   
   if(estaOculto){
-    conteudos[index].classList.remove('none')
-    conteudos[index].classList.add('conteudo-mostrado')
-    seta[index].classList.add('ativo')
-    return
-  }
-
-    conteudos[index].classList.add('none')
+      conteudos.forEach(function(item, index){
+      item.classList.remove('conteudo-mostrado')
+      seta[index].classList.remove('ativo')
+      })
+      conteudos[index].classList.add('conteudo-mostrado')
+      seta[index].classList.add('ativo')
+    
+  }else{    
+    conteudos[index].classList.remove('conteudo-mostrado')
     seta[index].classList.remove('ativo')
-   
+  }
   })
 })
 
